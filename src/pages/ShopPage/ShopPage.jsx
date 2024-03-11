c874158ac00d7b0ea67bba2f1939003b06f8ce91
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-03-12 00:12:58 +0200

    feat: update ShopPage

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,10 +1,106 @@
-import { Container } from "./ShopPage.styled";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectListDrugs,
+  selectIsLoading,
+  selectError,
+  selectSelectedShop,
+} from "../../redux/selectors";
+import { setSelectedShop } from "../../redux/drugsSlice";
+import { fetchListDrugs } from "../../redux/drugsOperations";
+import DrugCard from "../../components/DrugCard/DrugCard";
+import {
+  Container,
+  DivShops,
+  DivDrugs,
+  ListDrugs,
+  BtnShop,
+} from "./ShopPage.styled";
 
 const ShopPage = () => {
+  const dispatch = useDispatch();
+  const listDrugs = useSelector(selectListDrugs);
+  const error = useSelector(selectError);
+  const isLoading = useSelector(selectIsLoading);
+  const selectedShop = useSelector(selectSelectedShop);
+
+  useEffect(() => {
+    dispatch(fetchListDrugs(selectedShop));
+  }, [dispatch, selectedShop]);
+
+  const handleShopChange = (shop) => {
+    dispatch(setSelectedShop(shop));
+  };
+
+  const isShowList = listDrugs.length > 0;
+
   return (
     <section>
       <Container>
-        <h2>==Shop Page==</h2>
+        <DivShops>
+          <h3>Shops:</h3>
+          <BtnShop
+            onClick={() => handleShopChange("Drugs 24")}
+            $isActive={selectedShop === "Drugs 24"}
+          >
+            Drugs 24
+          </BtnShop>
+
+          <BtnShop
+            onClick={() => handleShopChange("Pharmacy")}
+            $isActive={selectedShop === "Pharmacy"}
+          >
+            Pharmacy
+          </BtnShop>
+
+          <BtnShop
+            onClick={() => handleShopChange("E-Tabletka")}
+            $isActive={selectedShop === "E-Tabletka"}
+          >
+            E-Tabletka
+          </BtnShop>
+
+          <div>etc...</div>
+        </DivShops>
+
+        <DivDrugs>
+          {isShowList && (
+            <ListDrugs>
+              {listDrugs.map((drug) => (
+                <li key={drug._id}>
+                  <DrugCard drug={drug} />
+                </li>
+              ))}
+            </ListDrugs>
+          )}
+
+          {!isShowList && (
+            <div
+              style={{
+                textAlign: "center",
+                fontSize: "16px",
+                fontWeight: 500,
+                margin: "100px auto 0",
+              }}
+            >
+              There are no drugs in the store yet!
+            </div>
+          )}
+
+          {isLoading && !error && (
+            <div
+              style={{
+                textAlign: "center",
+                fontSize: "16px",
+                fontWeight: 500,
+                margin: "100px auto 0",
+              }}
+            >
+              Request in progress...
+            </div>
+          )}
+          {error && error}
+        </DivDrugs>
       </Container>
     </section>
   );
--- src/pages/ShopPage/ShopPage.styled.jsx
+++ src/pages/ShopPage/ShopPage.styled.jsx
@@ -2,10 +2,82 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   max-width: 1200px;
+
+  display: flex;
+  gap: 20px;
+
   margin: 0 auto;
   margin-top: 70px;
-  padding: 150px 8px 150px;
+  padding: 20px 8px 40px;
 
-  outline: 2px solid red;
+  // outline: 2px solid red;
   // outline - рамка для обведення контенту
+`;
+
+export const DivShops = styled.div`
+  min-width: 215px;
+  height: 740px;
+
+  display: flex;
+  gap: 32px;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 32px;
+
+  border: 2px solid var(--primary-grey-color);
+  border-radius: 14px;
+`;
+
+export const DivDrugs = styled.div`
+  flex-grow: 1;
+  height: 740px;
+
+  padding: 32px;
+
+  overflow-y: auto;
+
+  border: 2px solid var(--primary-grey-color);
+  border-radius: 14px;
+`;
+
+export const ListDrugs = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  row-gap: 50px;
+  column-gap: 20px;
+`;
+
+export const BtnShop = styled.button`
+  cursor: pointer;
+
+  width: 100%;
+  max-width: 160px;
+  height: 44px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  text-decoration: none;
+
+  color: var(--primary-white-color);
+  // background-color: var(--accent-color);
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px; /* 142.857% */
+
+  border-radius: 12px;
+  border: none;
+
+  transition: background-color var(--transition);
+
+  &:hover,
+  &:focus {
+    background-color: var(--accent-hover-color);
+  }
+
+  background-color: ${(props) =>
+    props.$isActive ? "var(--accent-active-color)" : "var(--accent-color)"};
 `;
